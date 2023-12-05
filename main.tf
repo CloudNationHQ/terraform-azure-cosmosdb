b90df1c39a9a1389a5abf37feedcd5ae7eb166ba
Jorrit05 <jorstut@hotmail.com>
2023-12-05 15:39:08 +0100

    ipdate

--- main.tf
+++ main.tf
@@ -46,8 +46,8 @@ resource "azurerm_cosmosdb_account" "db" {
   ip_range_filter = var.cosmosdb.ip_range_filter
 
   virtual_network_rule {
-    id                                   = var.cosmosdb.subscription_id
-    ignore_missing_vnet_service_endpoint = try(var.cosmosdb.enable.ignore_missing_vnet_service_endpoint, false)
+    id                                   = try(var.cosmosdb.virtual_network_rule.id, "")
+    ignore_missing_vnet_service_endpoint = try(var.cosmosdb.virtual_network_rule.ignore_missing_vnet_service_endpoint, false)
   }
 }
 
